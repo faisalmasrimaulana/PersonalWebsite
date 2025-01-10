82feac6f3b79f6de5502208d8a28c461a854928f
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-01-10 21:17:36 +0700

    memperbaiki responsifitas

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // animasi typing
 let i = 0;
 let charHome = $(".type u");
+let munculHome = $(".munculHome");
 let border = $(".border")
 let oneCharHome = charHome.text();
 let speed = 50;
 charHome.text("");
 function typingHome() {
-
   if (i < oneCharHome.length) {
     charHome.html(charHome.html() + oneCharHome.charAt(i));
     i++;
     setTimeout(typingHome, speed);
   }
 }
-
 typingHome();
 charHome.addClass("animationBlinkLine");
 // if (i == oneCharHome.length) {
